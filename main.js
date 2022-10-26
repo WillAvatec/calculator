93f8bffc6d94ba3bba9ca1a6cb320647be7f462a
Will <avatec2312@gmail.com>
2022-10-26 01:46:55 -0500

    Adding operate function

---
+++ main.js
@@ -0,0 +1,23 @@
+//Estas son las funciones basicas
+
+function add (a,b){
+    return a+b;
+}
+
+function substract(a,b){
+    return a-b;
+}
+
+function multiply (a,b){
+    return a*b;
+}
+function divide (a,b){
+    return a/b;
+}
+
+
+// Esta funcion toma un operador y 2 numeros, 
+//  entonces llama una (FUNCION BASICA) y la aplica sobre los numeros. 
+
+function operate(num1, num2) {
+}
