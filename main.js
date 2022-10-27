c882443c752e1e9f96fd215a152e30eeb7e0ad97
Will <avatec2312@gmail.com>
2022-10-26 21:27:33 -0500

    Starting Again...

--- main.js
+++ main.js
@@ -20,4 +20,19 @@ function divide (a,b){
 //  entonces llama una (FUNCION BASICA) y la aplica sobre los numeros. 
 
 function operate(op, num1, num2) {
+    if (op == "+") return add(num1,num2);
+    if (op == "-") return substract(num1, num2);
+    if (op == "*") return multiply(num1, num2);
+    if (op == "/") return divide(num1, num2);
+
+    return "Unvalid Operation";
 }
+
+// Aqui estan todos las referencias al DOM 
+
+const display = document.querySelector(".actual-value");
+const numbers = document.querySelectorAll(".numbers button");
+const operators = document.querySelectorAll(".operators button");
+
+// When clicking over a button show the text in the display
+
